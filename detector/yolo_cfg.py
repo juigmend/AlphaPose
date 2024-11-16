ebe6e55d79c2ba0f90bb506c6228b2d30d021c9a
Juan Ignacio Mendoza Garay <juigmend@gmail.com>
2024-11-16 14:56:43 +0200

    16 Nov 2024

--- detector/yolo_cfg.py
+++ detector/yolo_cfg.py
@@ -4,6 +4,6 @@ cfg = edict()
 cfg.CONFIG = 'detector/yolo/cfg/yolov3-spp.cfg'
 cfg.WEIGHTS = 'detector/yolo/data/yolov3-spp.weights'
 cfg.INP_DIM = 608 # default = 608, (fast = 420)
-cfg.NMS_THRES = 0.45 # default = 0.6, fast = 0.45
-cfg.CONFIDENCE = 0.5 # default = 0.1, fast = 0.5
+cfg.NMS_THRES = 0.6 # default = 0.6, fast = 0.45
+cfg.CONFIDENCE = 0.1 # default = 0.1, fast = 0.5
 cfg.NUM_CLASSES = 80 # default = 80
